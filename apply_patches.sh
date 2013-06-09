3c298a2b25efdb6794b3798d1a5f82555c11ea17
mkovachev <mkovachev@2c3cb327-206d-4b14-bd95-7daaa68b26be>
2013-06-09 15:18:04 +0000

    Changed: all deps compile properly now

--- apply_patches.sh
+++ apply_patches.sh
@@ -7,6 +7,6 @@
 
 echo "Patching deps..."
 
-patch -p1 -i patches/libmp3lame_configure_patch_darwin_xmms_problem_fix_3.99.5.patch &&
-patch -p1 -i patches/libogg_configure_patch_darwin_optimizations_example_disabled_1.3.0 &&
+#patch -p1 -i patches/libmp3lame_configure_patch_darwin_xmms_problem_fix_3.99.5.patch &&
+#patch -p1 -i patches/libogg_configure_darwin.patch 
 patch -p1 -i patches/libvorbis_configure_patch_darwin_disabled_1.3.2.patch
--- combine.sh
+++ combine.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+source $(dirname $0)/functions.sh
+
+LIPO=lipo
+ARMV7S="`pwd`/tools/armv7sconvert/armv7sconvert.sh"
+PREFIX=$(realpath "$1")
+SELECTED_LIB=$2
+ARCHS=${ARCHS:-"armv7 i386"}
+
 set -e
 
-if [ -z "$ARCHS" ]; then
-    export ARCHS="armv7 i386"
-fi
-
-if [ -z "$SHAREDLIBS" ]; then
-    export SHAREDLIBS="/opt/ios"
-fi
-
-if [ -z "$SDKVER" ]; then
-    export SDKVER="6.1"
+if [ ! -d $PREFIX ]; then
+    echo "You must provide the base build path to the built libraries as the first parameter"
+    exit 1
 fi
 
 # start
 
 for ARCH in $ARCHS
 do
-  if [ -d "$SHAREDLIBS/$SDKVER/$ARCH" ]
+  if [ -d "$PREFIX/$ARCH" ]
   then
     MAIN_ARCH=$ARCH
   fi
@@ -30,29 +31,60 @@ then
   exit 1
 fi
 
-OUTPUT_DIR="$SHAREDLIBS/$SDKVER/fat"
+OUTPUT_DIR="$PREFIX/fat"
 rm -rf $OUTPUT_DIR
 
 mkdir -p $OUTPUT_DIR/lib $OUTPUT_DIR/include
 
-for LIB in $SHAREDLIBS/$SDKVER/$MAIN_ARCH/lib/*.a
+for LIB in $PREFIX/$MAIN_ARCH/lib/*.a
 do
+
   LIB=`basename $LIB`
+
+  if [ ! -z "$SELECTED_LIB" ] ; then
+    if [ "$LIB" != "$SELECTED_LIB" ] ; then
+	continue
+    fi
+  fi
+
   LIPO_CREATE=""
 
   for ARCH in $ARCHS
   do
-    if [ -d "$SHAREDLIBS/$SDKVER/$ARCH" ]
+    if [ -d "$PREFIX/$ARCH" ]
     then
-      LIPO_CREATE="$LIPO_CREATE-arch $ARCH $SHAREDLIBS/$SDKVER/$ARCH/lib/$LIB "
+        LIPO_CREATE="$LIPO_CREATE-arch $ARCH $PREFIX/$ARCH/lib/$LIB "	
     fi
   done
   OUTPUT="$OUTPUT_DIR/lib/$LIB"
   echo "Creating: $OUTPUT"
 
-  lipo -create $LIPO_CREATE -output $OUTPUT
-  lipo -info $OUTPUT
+  $LIPO -create $LIPO_CREATE -output $OUTPUT
+  
+  [ $? -eq 0 ] || {
+    echo "Error while creating fat library"
+    exit 3
+  }
+  
+  # run the special armv7s script
+  if [[ "$ARCHS" == *"armv7"* ]] ; then
+    $ARMV7S "$OUTPUT_DIR/lib/$LIB"
+    
+    [ $? -eq 0 ] || {
+	echo "Error while creating armv7s slice"
+	exit 4
+    }
+  fi
+  
+  $LIPO -info $OUTPUT
 done
 
 echo "Copying headers from dist-$MAIN_ARCH..."
-cp -R $SHAREDLIBS/$SDKVER/$MAIN_ARCH/include $OUTPUT_DIR
+cp -R $PREFIX/$MAIN_ARCH/include $OUTPUT_DIR
+
+[ $? -eq 0 ] || {
+    echo "Could not copy the include headers"
+    exit 4
+}
+
+exit 0
